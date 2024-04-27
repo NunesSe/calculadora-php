7b3428daa4ef63d9a7d02b18567773acef164c6b
Felipe Nunes <felipenunesseixas@gmail.com>
2024-04-27 12:11:39 -0300

    Refatoração do codigo, mudança na criação e acesso das variavei, criação da função salvar e separação das funções de operações para um novo arquivo

--- calculadora.php
+++ calculadora.php
@@ -10,89 +10,108 @@
 </head>
 
 <body>
-    
+
     <?php
-    // Pegar variaveis
-    if (isset($_POST["num1"])) {
-        $num1 = $_POST["num1"];
-    }
-    
-    if (isset($_POST["num2"])) {
-        $num2 = $_POST["num2"];
-    }
-    
-    if (isset($_POST["operacao"])) {
-        $operacao = $_POST["operacao"];
-    }
-    
-    
-    // Funções de operação
-    function mostrarOperacoes($num1, $num2, $operacao) {
-        return "{$num1} {$operacao} {$num2} = ";
-    }
-
-    function somar($num1, $num2)
-    {
-        return mostrarOperacoes($num1, $num2, "+") . $num1 + $num2;
-    }
-    
-    function subtrair($num1, $num2)
-    {
-        return mostrarOperacoes($num1, $num2, "-") . $num1 - $num2;
-    }
-
-    function dividir($num1, $num2)
-    {
-        if ($num2 == 0) {
-            return "Erro - Operação invalida!";
-        }
-        return mostrarOperacoes($num1, $num2, "/") . $num1 / $num2;
-    }
-
-    function multiplicar($num1, $num2)
-    {
-        return mostrarOperacoes($num1, $num2, "*") . $num1 *  $num2;
-    }
-
-    function elevar($num1, $num2)
-    {
-        return  mostrarOperacoes($num1, $num2, "^") . pow($num1,  $num2);
-    }
-
-    function fatorar($num1)
-    {
-        if($num1 >= 0) {
-            $resultado = 1;
-            for ($i=1; $i <= $num1; $i++) { 
-                $resultado *= $i;
-            }
-            return "{$num1}! = {$resultado}";
-        }
-        return "ERRO";
-    }
-
-
-    // TODO Criar historico e suas funcoes
+    // Iniciar sessao
     session_start();
+
+    // Inicializacao de variaveis
+    $_SESSION["num1"] = 0;
+    $_SESSION["num2"] = 0;
+    // Impoprtar as operaçoes
+    require_once("operacoes.php");
+
+    // Criação do historico
     if (!isset($_SESSION['historico'])) {
         $_SESSION['historico'] = [];
     }
-    
     $historico = $_SESSION['historico'];
 
-    if(isset($_POST["limpar"])) {
-        $historico = [];
+    // Criacao da funcao limpar historico
+    if (isset($_POST["limpar"])) {
         $_SESSION["historico"] = [];
+        $historico = [];
     }
 
+    
+    // Verifica se calcular foi clicado 
+    if (isset($_POST["calcular"])) {
+        // Pega as variaveis
+        $_SESSION["num1"] = trim($_POST["num1"]);
+        $_SESSION["num2"] = trim($_POST["num2"]);
+        $_SESSION["operacao"] = $_POST["operacao"];
+        // Verifica qual operação foi selecionada e faz as operações
+        switch ($_SESSION["operacao"]) {
+            case '+':
+                $_SESSION["resultado"] = somar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '-':
+                $_SESSION["resultado"] = subtrair($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '/':
+                $_SESSION["resultado"] = dividir($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '*':
+                $_SESSION["resultado"] = multiplicar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '^':
+                $_SESSION["resultado"] = elevar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '!':
+                $_SESSION["resultado"] = fatorar($_SESSION["num1"]);
+                break;
+        }
+        // Atualizando o historico
+        array_push($historico, $_SESSION["resultado"]);
+        $_SESSION['historico'] = $historico;
+    }
 
-    // TODO Criar salvar conta e voltar a conta salva
+    // Criação da função salvar operação
+    if (isset($_POST["salvar"])) {
+        $_SESSION["num1"] = trim($_POST["num1"]);
+        $_SESSION["num2"] = trim($_POST["num2"]);
+        $_SESSION["operacao"] = $_POST["operacao"];
+        // Verifica qual operação foi selecionada e faz as operações
+        switch ($_SESSION["operacao"]) {
+            case '+':
+                $_SESSION["contaSalva"] = somar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '-':
+                $_SESSION["contaSalva"] = subtrair($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '/':
+                $_SESSION["contaSalva"] = dividir($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '*':
+                $_SESSION["contaSalva"] = multiplicar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '^':
+                $_SESSION["contaSalva"] = elevar($_SESSION["num1"], $_SESSION["num2"]);
+                break;
+            case '!':
+                $_SESSION["contaSalva"] = fatorar($_SESSION["num1"]);
+                break;
+        }
+    }
+
+    if (isset($_SESSION["contaSalva"])) {
+        $contaSalva = $_SESSION["contaSalva"];
+    } else {
+        $contaSalva = "";
+    }
+
+    // Criação da função recupear operação
+
     ?>
-    <h2>Calculadora em php </h2>
 
+    <h2>Calculadora em php </h2>
     <form method="post" action="">
-        <label for="num2">Primeiro numero: </label>
-        <input type="text" name="num1" value="<?php echo $num1 = isset($num1) ? $num1 : 0;?>">
+        <label for="num1">Primeiro numero: </label>
+        <input type="text" name="num1" value="
+        <?php
+        // Manter o valor após realizar operação 
+        echo $_SESSION['num1'];
+        ?>">
         <select name="operacao">
             <option value="+"> + </option>
             <option value="-"> - </option>
@@ -102,74 +121,51 @@
             <option value="!"> ! </option>
         </select>
         <label for="num2">Segundo numero: </label>
-        <input type="text" name="num2" value="<?php echo $num2 = isset($num2) ? $num2 : 0;?>"><br>
-        <input type="submit" value="Calcular">
+        <input type="text" name="num2" value="
+        <?php
+        // Manter o valor após realizar operação 
+        echo $_SESSION["num2"];
+        ?>">
+        <br>
+        <input type="submit" value="Calcular" name="calcular">
+        <input type="submit" value="Salvar Operação" name="salvar">
     </form>
+    <form method="post">
+        <input type="submit" value="Recuperar Operação" name="recuperar">
+        <input type="submit" value="Limpar Historico" name="limpar">
+    </form>
+    <div class="">
+        <p>
+            <?php
+            // Mostrar resultado da operação
+            echo $_SESSION["resultado"];
+            ?>
+        </p>
+    </div>
 
-        <div class="">
-            <p>
-                <?php
-                if (isset($operacao)) {
-                    switch ($operacao) {
-                        case '+':
-                            $resultado = somar($num1, $num2);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        case '-':
-                            $resultado = subtrair($num1, $num2);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        case '/':
-                            $resultado = dividir($num1, $num2);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        case '*':
-                            $resultado = multiplicar($num1, $num2);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        case '^':
-                            $resultado = elevar($num1, $num2);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        case '!':
-                            $resultado = fatorar($num1);
-                            echo $resultado;
-                            array_push($historico, $resultado);
-                            $_SESSION['historico'] = $historico;
-                            break;
-                        default:
-                            echo "Resultado invalido!";
-                            break;
-                    }
-                }
-                ?>
-            </p>
-        </div>
-        <div>
-            <form method="post">
-                <input type="submit" value="Limpar Historico" name="limpar">
-            </form>
-        </div>
-        <div>
-            <h2>Historico</h2>
-            <ul>
-                <?php 
-                foreach ($historico as $conta) {
-                    echo  "<li>" . $conta . "<br></li>";
-                }
-                ?>
-            </ul>
-        </div>
+
+
+
+
+    <div>
+        <p>Operação salva:
+            <?php
+            // Mostra a conta salva
+            echo $_SESSION["contaSalva"];
+            ?>
+        </p>
+    </div>
+    <div>
+        <h2>Historico</h2>
+        <ul>
+            <?php
+            // Mostar os itens dentro de historico
+            foreach ($historico as $conta) {
+                echo  "<li>" . $conta . "<br></li>";
+            }
+            ?>
+        </ul>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
---
+++ operacoes.php
@@ -0,0 +1,47 @@
+<?php 
+// Funções de operação
+function mostrarOperacoes($num1, $num2, $operacao)
+{
+    return "{$num1} {$operacao} {$num2} = ";
+}
+
+function somar($num1, $num2)
+{
+    return mostrarOperacoes($num1, $num2, "+") . $num1 + $num2;
+}
+
+function subtrair($num1, $num2)
+{
+    return mostrarOperacoes($num1, $num2, "-") . $num1 - $num2;
+}
+
+function dividir($num1, $num2)
+{
+    if ($num2 == 0) {
+        return "Erro - Operação invalida!";
+    }
+    return mostrarOperacoes($num1, $num2, "/") . $num1 / $num2;
+}
+
+function multiplicar($num1, $num2)
+{
+    return mostrarOperacoes($num1, $num2, "*") . $num1 *  $num2;
+}
+
+function elevar($num1, $num2)
+{
+    return  mostrarOperacoes($num1, $num2, "^") . pow($num1,  $num2);
+}
+
+function fatorar($num1)
+{
+    if ($num1 >= 0) {
+        $resultado = 1;
+        for ($i = 1; $i <= $num1; $i++) {
+            $resultado *= $i;
+        }
+        return "{$num1}! = {$resultado}";
+    }
+    return "ERRO";
+}
+?>
